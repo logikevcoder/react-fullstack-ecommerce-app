7d9b652fd78dd6ff72432701862756f7ade38ed3
Kevin Napier <ragnarok@Kevins-MacBook-Pro.local>
2018-10-25 18:53:55 -0500

    added passport routes for authentication of user google account

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,4 @@
 module.exports = {
-    googleClientId: 583728236459-ru6u6nlj2ig3blpnarfd692btifg0968.apps.googleusercontent.com,
-    googleClientSecret: Q4zEskP_esGZ8vwR6vb_WoMq
+    googleClientID: '583728236459-28id0dbeqflm8go8qem45rjoip21tlbq.apps.googleusercontent.com',
+    googleClientSecret: '1xl99DJPIa3LYKXEKtWE6js0'
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,38 @@
-const express = require('express'); // older way of importing.. newer way is import express from 'express';
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require("express"); // older way of importing.. newer way is import express from 'express';
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/key");
 
 const app = express();
 
 // creating a new google strategy authentication with google
-passport.use(new GoogleStrategy()); 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID, // grabs clientID from keys.js file
+      clientSecret: keys.googleClientSecret, // grabs ClientSecret from keys.js file
+      callbackURL: '/auth/google/callback' // route request for the user
+    },
+    (accessToken, refreshToken, profile, done) => {
+
+      console.log('access token', accessToken);
+      console.log('Refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+// route to turn clients request into a profile, rather than an initial authentication
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log('Server is running');
+});
