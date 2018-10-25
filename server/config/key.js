780fa0990188c4f222ae489ddccc80625bed16ce
Kevin Napier <ragnarok@Kevins-MacBook-Pro.local>
2018-10-25 15:47:16 -0500

    imported use of passport.js and passport-google-oath strategy

---
+++ server/config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientId: 583728236459-ru6u6nlj2ig3blpnarfd692btifg0968.apps.googleusercontent.com,
+    googleClientSecret: Q4zEskP_esGZ8vwR6vb_WoMq
+}
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express'); // older way of importing.. newer way is import express from 'express';
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'There' });
-});
+// creating a new google strategy authentication with google
+passport.use(new GoogleStrategy()); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
